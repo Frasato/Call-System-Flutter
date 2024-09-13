ec2694946aa577a98a791a327e86ba269bab8c70
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-09-13 10:18:50 -0300

    Call model to calss

---
+++ lib/models/call.dart
@@ -0,0 +1,9 @@
+class Call {
+  final String title;
+  final String subtitle;
+  final String time;
+  final String details;
+  final String anyDeskId;
+
+  Call(this.title, this.subtitle, this.details, this.time, this.anyDeskId);
+}
