def572cf69798a379be4d8960ecaf3743d46971c
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-09-13 10:18:06 -0300

    constants colors for project

---
+++ lib/constants/color.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color primaryYellow = Color(0xFFFEEE00);
+const Color greyColor = Color.fromARGB(15, 231, 231, 231);
+const Color lightGreyColor = Color.fromARGB(255, 149, 149, 149);
+const Color greyBackground = Color.fromARGB(255, 31, 31, 31);
