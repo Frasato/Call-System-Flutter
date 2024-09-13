8deaedf8f82c5a218cd4a0b2d7ab78ff3a516011
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-09-13 10:17:56 -0300

    main file

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:estudando_flutter/screen/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
